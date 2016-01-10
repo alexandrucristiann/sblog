203463d451863362e36ae4701588a241abc78210
kriszt <kriszt_24@yahoo.com>
2016-01-10 13:20:51 +0200

    fixed

--- lib/constants/system-constants.php
+++ lib/constants/system-constants.php
@@ -2,13 +2,13 @@
 
 define('SUBDIRECTORY',null);
 define('ROOT',str_replace('lib\constants','',realpath(dirname(__FILE__))));
-define('HANDLERS_PATH',ROOT.'/lib/handlers/');
-define('MODELS_PATH',ROOT.'/lib/models/');
-define('MODULES_PATH',ROOT.'/lib/modules/');
-define('APPLICATION_CONSTANTS',ROOT.'/lib/constants/');
-define('LOADERS',ROOT.'/lib/loaders/');
-define('COLLECTION_PATH',ROOT.'/lib/collections/');
-define('CONSTANTS_PATH',ROOT.'/lib/constants/');
-define('EXCEPTIONS_PATH',ROOT.'/lib/exceptions/');
+define('HANDLERS_PATH',str_replace("\\","/",ROOT.'lib\handlers\\'));
+define('MODELS_PATH',str_replace("\\","/",ROOT.'lib\models\\'));
+define('MODULES_PATH',str_replace("\\","/",ROOT.'lib\modules\\'));
+define('APPLICATION_CONSTANTS',str_replace("\\","/",ROOT.'lib\constants\\'));
+define('LOADERS',str_replace("\\","/",ROOT.'lib/loaders\\'));
+define('COLLECTION_PATH',str_replace("\\","/",ROOT.'lib\collections\\'));
+define('CONSTANTS_PATH',str_replace("\\","/",ROOT.'lib\constants\\'));
+define('EXCEPTIONS_PATH',str_replace("\\","/",ROOT.'lib\exceptions\\'));
 define('APP_PATH','http'.(isset($_SERVER['HTTPS'])?'s':'').'://'.$_SERVER['SERVER_NAME'].'/'.SUBDIRECTORY);
 define('APP_WEBSITE','http'.(isset($_SERVER['HTTPS'])?'s':'').'://'.$_SERVER['SERVER_NAME'].'/'.SUBDIRECTORY);
