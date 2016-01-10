b3d3f0aa992c6cd2775e32cdebe41b743c96ae65
kriszt <kriszt_24@yahoo.com>
2016-01-10 13:20:25 +0200

    fixed

--- lib/loaders/resources-loader.php
+++ lib/loaders/resources-loader.php
@@ -2,4 +2,6 @@
 
 require_once(HANDLERS_PATH.'sql-handler.php');
 
-require_once(CONSTANTS_PATH."database-constants.php");
+require_once(CONSTANTS_PATH.'database-constants.php');
+
+require_once(MODULES_PATH.'User.php');
