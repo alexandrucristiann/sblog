9cf97ec99bc2fcd30d9d80c448f2e2063f90019f
kriszt <kriszt_24@yahoo.com>
2016-01-10 05:38:37 +0200

    loaded exceptions, srsbsns

--- lib/loaders/exceptions-loader.php
+++ lib/loaders/exceptions-loader.php
@@ -1,3 +1,6 @@
 <?php
 
 require_once(EXCEPTIONS_PATH.'CriticalFaultException.php');
+require_once(EXCEPTIONS_PATH.'DuplicateEmailException.php');
+require_once(EXCEPTIONS_PATH.'InvalidLoginCredentials.php');
+require_once(EXCEPTIONS_PATH.'InvalidUniqueException.php');
