6ea56a2946ea740853a8dbac781c3068fab68150
kriszt <kriszt_24@yahoo.com>
2016-01-10 21:07:15 +0200

    loading loggedUser

--- lib/loaders/loader.php
+++ lib/loaders/loader.php
@@ -3,3 +3,16 @@
 require_once('exceptions-loader.php');
 require_once('resources-loader.php');
 require_once('handlers-loader.php');
+
+if(!empty($_SESSION['unique_user']))
+{
+    try {
+    $loggedUser = User::loginSession($_SESSION['unique_user']);
+    } catch (InvalidUniqueException $e) {
+        echo $e->getMessage();
+    } catch (InvalidLoginCredentials $e) {
+        echo $e->getMessage();
+    }
+}
+else
+    $loggedUser = null;
