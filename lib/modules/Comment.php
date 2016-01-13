4b70e9ec26416e71667b484108b0b974b69ab61f
kriszt <kriszt_24@yahoo.com>
2016-01-13 17:22:20 +0200

    created add() and delete() methods

--- lib/modules/Comment.php
+++ lib/modules/Comment.php
@@ -22,4 +22,28 @@ class Comment {
         $this->time = $data['time'];
     }
 
+    public static function add($uniquePost,$isUser,$uniqueUser,$guestName,$message,$email)
+    {
+        $uniqueComment = rand(100,999) . uniqid() . rand(100,999);
+        global $sql;
+        $checkUnique = $sql->query("SELECT `unique_comment` FROM `comments` WHERE `unique_comment`='" . $uniqueComment . "'");
+        if ($checkUnique->num_rows == 0)
+        {
+            try {
+                $sql->query("INSERT INTO `comments` (`unique_comment`,`unique_post`,`is_user`,`unique_user`,`guest_name`,`message`,`email`,`ip`,`time`) VALUES ('" . $uniqueComment . "','" . $uniquePost . "','" . $isUser . "','" . $uniqueUser . "','" . $guestName . "','" . $message. "','" . $email . "','" . $_SERVER['REMOTE_ADDR'] . "','" . date("Y-m-d H:i:s") . "')");
+                return new Comment($uniqueComment);
+            } catch (InvalidUniqueException $e) {
+                throw new CriticalFaultException($e->getMessage());
+            }
+        }
+        else
+            throw new CriticalFaultException();
+    }
+
+    public function delete()
+    {
+        global $sql;
+        $sql->query("DELETE FROM `comments` WHERE `unique_comment`='" . $this->uniqueComment . "'");
+    }
+
 }
