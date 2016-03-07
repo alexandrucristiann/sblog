bc3127e434e7e9184cb794092d4cb686065bd5e6
kriszt <kriszt_24@yahoo.com>
2016-03-07 17:01:00 +0200

    xx

--- lib/loaders/resources-loader.php
+++ lib/loaders/resources-loader.php
@@ -5,3 +5,6 @@ require_once(HANDLERS_PATH.'sql-handler.php');
 require_once(CONSTANTS_PATH.'database-constants.php');
 
 require_once(MODULES_PATH.'User.php');
+
+require_once(COLLECTION_PATH.'CommentsCollection.php');
+require_once(COLLECTION_PATH.'PostsCollection.php');
